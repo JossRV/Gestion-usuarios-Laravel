9480f3be475e75b7fa339916160e3980c311ded3
Joss Rv <jossrv74@gmail.com>
2024-10-21 01:10:37 -0600

    estructuraciond e proyecto

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,33 @@
+{{-- <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+
+        </div>
+    </nav>
+    <ul class="nav justify-content-center nav-underline bg-dark">
+        <li class="nav-item">
+            <a class="nav-link text-white" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="#">Link</a>
+        </li>
+    </ul> --}}
+<nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    @include('includes.header')
+</head>
+<body>
+    @include('includes.nav')
+    @yield('content')
+</body>
+</html>
